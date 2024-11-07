e13dfaba01eda686ad1b4bd27a8dbb5caca08b7d
Arseniy Velikanov <me@adomerle.pw>
2024-11-07 23:21:29 +0400

    mach-mediatek: Add initial support for MT6789
    Signed-off-by: Arseniy Velikanov <me@adomerle.pw>

---
+++ include/configs/mt6789.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for MT6789 based boards
+ *
+ * Copyright (C) 2024 Arseniy Velikanov
+ */
+
+#ifndef __MT6789_H
+#define __MT6789_H
+
+#endif
